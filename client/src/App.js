21d71e617dc8ce91eb957ac01e7423d8c4f11964
claytonsavage <clytnsavage@gmail.com>
2018-10-22 21:29:05 -0700

    modified starting code

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+//Import static files
+import './styles/App.css';
+import Navigation from './Navigation';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            The begining ends today
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React Clayton for fucks sakes
-          </a>
-        </header>
+      <div>
+          <Navigation />
       </div>
     );
   }
---
+++ client/src/Navigation.js
@@ -0,0 +1,12 @@
+import React, {
+    Component
+} from 'react';
+
+class App extends Component {
+    render() {
+        return ( <div>Your first component</div>
+        );
+    }
+
+}
+export default App;
